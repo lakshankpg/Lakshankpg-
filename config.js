b676335f86b519de72e3dfd385f6771bdc57ba82
lakshankpg <lakshankpg@gmail.com>
2025-07-19 07:58:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,1 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
-module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=wr8zmIiL#VdHh2psiYCN3-T13mbTNDzE285_LAU_kmOwrb7HryPk' : process.env.SESSION_ID,
-PREFIX: process.env.PREFIX || '.' ,
-SUDO: process.env.SUDO === undefined ? '94762731899' : process.env.SUDO,
-MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
-};
